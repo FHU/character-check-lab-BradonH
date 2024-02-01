89e0534061fc2e0fd6c986c43c938f87105f0792
BradonH <Bradon.hopper@students.fhu.edu>
2024-02-01 12:40:11 -0600

    update main.py
    push for grade

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def check_character(word, index):
         return "white space"
     else:
         return "unknown"
+#hello
 
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
